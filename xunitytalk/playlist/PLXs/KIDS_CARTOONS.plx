bae78b649e4c5195fa8706099acead6b1ace1f27
MzDiobolik <evilorphen@gmail.com>
2015-10-17 17:43:38 -0400

    commit

---
+++ xunitytalk/playlist/PLXs/KIDS_CARTOONS.plx
@@ -0,0 +1,134 @@
+version=1
+background=https://raw.github.com/mefrox/xunitytalks_playlist/master/xunity_playlist_art/xunity_bg_03.png
+title=[B][COLOR steelblue]X[/COLOR][COLOR gray]UNITY[/COLOR][COLOR steelblue]T[/COLOR][COLOR gray]ALK's[/COLOR][/B] [COLOR orange][B]Cartoons[/B][/COLOR] [COLOR white]and[/COLOR] [COLOR yellow][B]Kids[/B][/COLOR]
+logo=
+
+
+#1
+type=playlist
+name=[B][COLOR orange]Cartoons[/COLOR][/B] in [COLOR lime]HD[/COLOR]
+thumb=
+URL=https://xunitytalk-playlist-directory.googlecode.com/svn/trunk/XMLs/cartoon_HD.xml
+
+#2
+type=playlist
+name=[B][COLOR yellow]Lego[/COLOR][/B] [COLOR red]Videos[/COLOR]
+thumb=https://lh4.googleusercontent.com/-3X29Anw4TwM/Tw1x3HzZdvI/AAAAAAAAJxs/A2EZFpNrpTI/w480-h479-no/LEGO_LOGO_RGB.PNG
+URL=http://gdata.youtube.com/feeds/api/users/LEGO/playlists
+
+#3
+type=playlist
+name=[B][COLOR red]M[/COLOR][COLOR yellow]i[/COLOR][COLOR magenta]k[/COLOR][COLOR lime]e[/COLOR][COLOR white]y[/COLOR][COLOR orange]1[/COLOR][COLOR cyan]2[/COLOR][COLOR red]3[/COLOR][COLOR yellow]4[/COLOR] [COLOR magenta]P[/COLOR][COLOR lime]l[/COLOR][COLOR white]a[/COLOR][COLOR orange]y[/COLOR][COLOR cyan]L[/COLOR][COLOR red]i[/COLOR][COLOR yellow]s[/COLOR][COLOR magenta]t[/COLOR][/B]
+thumb=http://mikey1234-repo.googlecode.com/svn/icon.png
+URL=https://github.com/MzDiobolik/Playlist/mikey1234 
+
+#4
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#5
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#6
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#7
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#8
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#9
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#10
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#11
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#12
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#13
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#14
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#15
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#16
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#17
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#18
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#19
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#20
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#21
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#22
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#23
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#24
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
+#25
+type=playlist
+name=[COLOR gray]Available Slot[/COLOR]
+thumb=
+URL= 
---
+++ xunitytalk/playlist/cartoonhd/GRAB LINKS.py
@@ -0,0 +1,67 @@
+import urllib2,re,os
+
+import json
+
+
+ 
+def OPEN_URL(url):
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    link=response.read()
+    response.close()
+    return link
+
+FIRST='<poster>[B][COLOR cyan].[/COLOR][COLOR red]M[/COLOR][COLOR yellow]i[/COLOR][COLOR magenta]k[/COLOR][COLOR lime]e[/COLOR][COLOR white]y[/COLOR][COLOR orange]1[/COLOR][COLOR cyan]2[/COLOR][COLOR red]3[/COLOR][COLOR yellow]4[/COLOR] [COLOR magenta]P[/COLOR][COLOR lime]l[/COLOR][COLOR white]a[/COLOR][COLOR orange]y[/COLOR][COLOR cyan]L[/COLOR][COLOR red]i[/COLOR][COLOR yellow]s[/COLOR][COLOR magenta]t[/COLOR][/B]</poster>\n<thumbnail>https://github.com/MzDiobolik/Playlist/xunitytalk/icon.png</thumbnail>\n<fanart>https://github.com/MzDiobolik/Playlist/xunitytalk/fanart.jpg</fanart>\n\n'
+f = open('playlist', mode='w')
+f.write(FIRST)
+f.close()
+
+
+a=OPEN_URL('http://gappcenter.com/app/cartoon/mapi.php?action=getcategory&os=newiosfull&version=2.1&deviceid=537BE108497E67A84974CD560691D906&token=&time=&device=iphone')
+link=json.loads(a)
+
+data=link['Data']
+
+for field in data:
+    name= field['Name'].encode("utf-8")
+    iconimage= field['Image'].encode("utf-8")
+    
+    action=field['Action'].encode("utf-8")
+    if 'Top' in name:
+        name='.[COLOR cyan]%s[/COLOR]'% name
+    if 'Disney' in name:
+        name='.[COLOR cyan]%s[/COLOR]'% name
+
+
+        
+    DIR='<name>%s</name>\n<link>https://github.com/MzDiobolik/Playlist/xunitytalk/playlist/cartoonhd/%s.xml</link>\n<thumbnail>%s</thumbnail>\n\n' % (name,action,iconimage)
+    f = open('playlist', mode='a')
+    f.write(DIR)
+    f.close()
+    
+    SECOND='<poster>[B][COLOR cyan].[/COLOR][COLOR red]M[/COLOR][COLOR yellow]i[/COLOR][COLOR magenta]k[/COLOR][COLOR lime]e[/COLOR][COLOR white]y[/COLOR][COLOR orange]1[/COLOR][COLOR cyan]2[/COLOR][COLOR red]3[/COLOR][COLOR yellow]4[/COLOR] [COLOR magenta]P[/COLOR][COLOR lime]l[/COLOR][COLOR white]a[/COLOR][COLOR orange]y[/COLOR][COLOR cyan]L[/COLOR][COLOR red]i[/COLOR][COLOR yellow]s[/COLOR][COLOR magenta]t[/COLOR][/B]</poster>\n<thumbnail>https://github.com/MzDiobolik/Playlist/xunitytalk/icon.png</thumbnail>\n<fanart>https://github.com/MzDiobolik/Playlist/xunitytalk/fanart.jpg</fanart>\n\n'
+    f = open(action+'.xml', mode='w')
+    f.write(SECOND)
+    f.close()
+    new_url='http://gappcenter.com/app/cartoon/mapi.php?action=getlistcontent&cate=%s&pageindex=0&pagesize=1000&os=newiosfull&version=2.2&deviceid=537BE108497E67A84974CD560691D906&token=63EFF51FE2236915658D502AE089350C&time=1389744589&device=iphone'%action
+    response=OPEN_URL(new_url)
+    
+    if not 'invalid token' in response:
+        DATA=json.loads(response)
+
+        data=DATA['Data']
+
+        for field in data:
+            name= field['Name'].encode("utf-8")
+            iconimage= field['Image'].encode("utf-8")
+            url=field['Link'].encode("utf-8")
+
+            
+            action_='<item>\n<title>%s</title>\n<link>%s</link>\n<thumbnail>%s</thumbnail>\n</item>\n\n'%(name,url,iconimage)
+            f = open(action+'.xml', mode='a')
+            f.write(action_)
+            f.close() 
+        
+
+print 'FINISHED'
